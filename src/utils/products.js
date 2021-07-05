b8c1f2ca01c59597ff92bd0100f78e755678761f
hamayun <hamayun.waheed1999@gmail.com>
2021-07-05 18:19:22 +0500

    add utility function

---
+++ src/utils/products.js
@@ -0,0 +1,11 @@
+async function getUsers() {
+  try {
+    const result = await fetch('./db/db.json');
+    const data = await result.json();
+    return data;
+  } catch(e) {
+    return null;
+  }
+}
+
+export default getUsers;
