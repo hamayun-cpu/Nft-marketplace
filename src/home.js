65a0f7fef0205378dab58fd0f64ba06650b69f3a
hamayun <hamayun.waheed1999@gmail.com>
2021-07-15 11:04:48 +0500

    update home and update component to make them cleaner

--- src/home.js
+++ src/home.js
@@ -5,27 +5,29 @@ import Countdown from 'react-countdown';
 import ReactPlayer from 'react-player';
 import timeHelper from './utils/timeHelper'
 
-const renderer = ({ hours, minutes, seconds, completed }) => {
-  if (completed) {
-    return "biding Ended";
-  } else {
-    return <span>{hours}:{minutes}:{seconds}</span>;
-  }
-};
 
 function Home() {
+  //variables
   const { data: Salesnfts, error } = useSWR('./utils/products.js', getSalesNft);
   const bidNfts = getBidNft();
 
+  //try catching data/error
   if(error) return "Error!";
   if(!Salesnfts) return "Loading!";
 
-    return (
+  //functions
+  const renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+      return "biding Ended";
+    } else {
+      return <span>{hours}:{minutes}:{seconds}</span>;
+    }
+  };
+
+  return (
     <div className="mt-12">
       <div className="d-flex justify-content-center">
-        <h1>
-          Sales
-        </h1>
+        <h1>Sales</h1>
       </div>
       <div className="container">
         <div className="row">
@@ -55,28 +57,26 @@ function Home() {
         </div>
       </div>
       <div className="d-flex justify-content-center mt-5">
-        <h1>
-          Bids
-        </h1>
+        <h1>Bids</h1>
       </div>
       <div className="container mb-2">
         <div className="row">
           {bidNfts.map((nft) => (
             <div className="col-sm mt-3" key={nft.id}>
               <div className="card w-o h-550 m-auto">
-              { nft.type === 'pic' &&
-                <img className="card-img-top h-60" src={nft.media}  alt="Card cap"/>
-              }
+                { nft.type === 'pic' &&
+                  <img className="card-img-top h-60" src={nft.media}  alt="Card cap"/>
+                }
 
-              { nft.type === 'vid'  &&
-                <ReactPlayer className="card-img-top h-60" url={nft.media}
-                  controls = {false}
-                  muted = {true}
-                  width = {'20.9rem'}
-                  playing = {true}
-                  loop = {true}
-                />
-              }
+                { nft.type === 'vid'  &&
+                  <ReactPlayer className="card-img-top h-60" url={nft.media}
+                    controls = {false}
+                    muted = {true}
+                    width = {'20.9rem'}
+                    playing = {true}
+                    loop = {true}
+                  />
+                }
                 <div className="card-body">
                   <h5 className="card-title"><a href={`/product/${nft.id}`} className="color-black">{nft.title}</a></h5>
                   <p className="card-text">{nft.description}</p>
@@ -93,8 +93,7 @@ function Home() {
         </div>
       </div>
     </div>
-    );
-  
+  );
 }
 
 export default Home;
--- src/update.js
+++ src/update.js
@@ -4,13 +4,15 @@ import { fuzzySearch } from "react-select-search";
 import { useState } from "react";
 
 const Update = () => {
+  //varibales
   let ids = getAllIds();
-
   const [toggleValue, setStatus] = useState("");
   const [bidPrice, setBidPrice] = useState("");
   const [salePrice, setSalePrice] = useState("");
   const [idd, setIdd] = useState("");
 
+
+  //funtions
   function toggle() {
     if (toggleValue) {
       setStatus(false);
@@ -43,24 +45,22 @@ const Update = () => {
     }
   }
 
+  //JSX
   return (
     <div className="d-flex justify-content-center mt-5 pad-75">
       <form className = 'd-flex flex-column align-items-center border rounded p-5' onSubmit={handleSubmit}>
-      <h3 className="mb-5">Convert Nft to Sale/Bid</h3>
+        <h3 className="mb-5">Convert Nft to Sale/Bid</h3>
         <SelectSearch
           options={ids}
           search
           filterOptions={fuzzySearch}
           placeholder="Select Nft by Id"
-        
           onChange={ setIdd} 
         />
-
         <label className="mt-3">
           <input className="mx-2" type="checkbox" id="myCheck" onChange={ (event) => toggle()}/>
           For bid?
         </label> <br></br>
-
         {toggleValue &&
           <label>
             Bid Price: 
@@ -69,7 +69,6 @@ const Update = () => {
           </label>
         }
         <br></br>
-
         {!toggleValue &&
           <label>
             Sale Price: 
@@ -77,9 +76,7 @@ const Update = () => {
             ETH
           </label>
         }
-
         <br></br>
-
         <input type="submit" value="Submit" className="btn btn-primary w-100"/>
       </form>
    </div>
