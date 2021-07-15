bd9764a2708bf8fc12fd18553ae4318f32c200c2
hamayun <hamayun.waheed1999@gmail.com>
2021-07-15 11:10:48 +0500

    update product component to make code cleaner

--- src/product.js
+++ src/product.js
@@ -6,20 +6,20 @@ import timeHelper from './utils/timeHelper'
 import Popup from 'reactjs-popup';
 import { useState } from 'react';
 
-const renderer = ({ hours, minutes, seconds, completed }) => {
-  if (completed) {
-    return "biding Ended";
-  } else {
-    return <span>{hours}:{minutes}:{seconds}</span>;
-  }
-};
-
 const Product = (props) => {
-
+  //variables
   let { id } = useParams();
   const nft = getUserById(id);
-
   const [bidPrice, setBid] = useState("");
+
+  //functions
+  const renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+      return "biding Ended";
+    } else {
+      return <span>{hours}:{minutes}:{seconds}</span>;
+    }
+  };
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -31,15 +31,13 @@ const Product = (props) => {
     }
   }
 
+  //JSX
   return (
-
     <div className = "mt-12">
       <div className="card w-o h-100 m-auto">
-
         { nft.type === 'pic' &&
           <img className="card-img-top h-50" src={nft.media}  alt="Card cap"/>
         }
-
         { nft.type === 'vid'  &&
           <ReactPlayer className="card-img-top h-50" url={nft.media}
             controls = {true}
@@ -48,17 +46,17 @@ const Product = (props) => {
             playing = {true}
           />
         }
-        
         <div className="card-body">
           <h5 className="card-title">{nft.title}</h5>
           <p className="card-text"> {nft.description}</p>
           <div className="d-flex justify-content-between">
             <button className="btn btn-primary">{nft.price}</button>
-            {!nft.buyable &&  <span className="btn btn-info">
-              <Countdown date={timeHelper(nft.time)} renderer={renderer} ></Countdown>
-            </span>}
+            {!nft.buyable &&
+              <span className="btn btn-info">
+                <Countdown date={timeHelper(nft.time)} renderer={renderer} ></Countdown>
+              </span>
+            }
           </div>
-
           {nft.buyable &&
             <Popup trigger={<button className="btn btn-primary mt-2"> Buy</button>}>
               <div className = "popup d-flex justify-content-center align-items-center flex-column">
@@ -67,7 +65,6 @@ const Product = (props) => {
               </div>
             </Popup>
           }
-
           {!nft.buyable &&
            <Popup trigger={<button className="btn btn-primary mt-2"> Bid</button>}>
               <div className = "popup d-flex justify-content-center align-items-center flex-column">
@@ -80,60 +77,59 @@ const Product = (props) => {
               </div>
             </Popup>
           }
-
         </div>
       </div>
       {!nft.buyable &&
-      <table className='mx-auto my-5'>
-        <tr>
-          <th>Highest bid</th>
-          <th>From</th>
-        </tr>
-        <tr>
-          <td>20 ETH</td>
-          <td>0x1235653234</td>
-        </tr>
-        <tr>
-          <td>20 ETH</td>
-          <td>0x1235653234</td>
-        </tr>
-        <tr>
-          <td>20 ETH</td>
-          <td>0x1235653234</td>
-        </tr>
-        <tr>
-          <td>20 ETH</td>
-          <td>0x1235653234</td>
-        </tr>
-      </table>
+        <table className='mx-auto my-5'>
+          <tr>
+            <th>Highest bid</th>
+            <th>From</th>
+          </tr>
+          <tr>
+            <td>20 ETH</td>
+            <td>0x1235653234</td>
+          </tr>
+          <tr>
+            <td>20 ETH</td>
+            <td>0x1235653234</td>
+          </tr>
+          <tr>
+            <td>20 ETH</td>
+            <td>0x1235653234</td>
+          </tr>
+          <tr>
+            <td>20 ETH</td>
+            <td>0x1235653234</td>
+          </tr>
+        </table>
       }
       {nft.buyable &&
-      <table className='mx-auto my-5'>
-        <tr>
-          <th>Sale History</th>
-          <th>From</th>
-          <th>To</th>
-        </tr>
-        <tr>
-          <td>20 ETH</td>
-          <td>0x1235653234</td>
-          <td>0x1235653edc</td>
-        </tr>
-        <tr>
-          <td>20 ETH</td>
-          <td>0x1235653234</td>
-          <td>0x1235653edc</td>
-        </tr>
-        <tr>
-          <td>20 ETH</td>
-          <td>0x1235653234</td>
-          <td>0x1235653edc</td>
-        </tr>
-        <tr>
-          <td>20 ETH</td>
-          <td>0x1235653234</td>
-          <td>0x1235653edc</td>
-        </tr>
+        <table className='mx-auto my-5'>
+          <tr>
+            <th>Sale History</th>
+            <th>From</th>
+            <th>To</th>
+          </tr>
+          <tr>
+            <td>20 ETH</td>
+            <td>0x1235653234</td>
+            <td>0x1235653edc</td>
+          </tr>
+          <tr>
+            <td>20 ETH</td>
+            <td>0x1235653234</td>
+            <td>0x1235653edc</td>
+          </tr>
+          <tr>
+            <td>20 ETH</td>
+            <td>0x1235653234</td>
+            <td>0x1235653edc</td>
+          </tr>
+          <tr>
+            <td>20 ETH</td>
+            <td>0x1235653234</td>
+            <td>0x1235653edc</td>
+          </tr>
       </table>
       }
     </div>
